f072a7761878291a5d6e76e18d1630274b24bdb3
Mustafa Raad <mustf.raad@gmail.com>
2024-01-24 06:54:24 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div>
+
+    </div>
+  </div>
+);
+
+export default App
