27107911adac7c9a51a2c413c87bc4e1fbeefe71
Mustafa Raad <mustf.raad@gmail.com>
2024-01-24 07:18:54 +0300

    Set The Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import styles from "./style";
+
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testomonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
 
 const App = () => (
-  <div className='bg-primary w-full overflow-hidden'>
-    <div>
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={styles.boxWidth}>
+        <Navbar />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
 
+    <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testomonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
     </div>
   </div>
 );
 
-export default App
+export default App;
