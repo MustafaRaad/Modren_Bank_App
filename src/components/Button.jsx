b0e3578de02ae09a0f4308a28e6f1ae3297ebff5
Mustafa Raad <mustf.raad@gmail.com>
2024-02-02 12:24:15 +0300

    Btn Cmpnt

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+const Button = ({ styles, text }) => (
+  <button
+    type="button"
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+  >
+    {text}
+  </button>
+);
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
-
-export default Button
+export default Button;
